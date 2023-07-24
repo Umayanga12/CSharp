efbe98ae7f21f0c3b9949d42be8494d2d5e2e523
Umayanga Athapaththu <42837553+Umayanga12@users.noreply.github.com>
2023-07-24 23:34:23 +0530

    Add files via upload

---
+++ lab04/kmTom/ConvertValues.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kmTom
+{
+    internal class ConvertValues
+    {
+        public float KilometerTometer(float km)
+        {
+            float meter = km * 1000;
+            return meter;
+        }
+
+    }
+}
---
+++ lab04/kmTom/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kmTom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Object creation
+            ConvertValues convert = new ConvertValues();
+
+            //Getting meter value from the user
+            Console.WriteLine("Enter Your distance in kilometer: ");
+            float km = (float) Convert.ToDecimal(Console.ReadLine());
+
+            float meter = convert.KilometerTometer(km);
+            Console.WriteLine("Your distance in Kilometer is: " + meter);
+
+
+            Console.ReadKey();
+        }
+    }
+}
